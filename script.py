ab1ffe0c769ffbd52a49788aa651bd0bf4148be0
hamogu <moritz.guenther@gmx.de>
2011-04-11 17:12:02 -0400

    improved script examples

--- script.py
+++ script.py
@@ -1,6 +1,15 @@
 import NX2
-#dat = NX2.NX2Table('../data/18tue_firstday.00.csv', (18,5,2009))
+
 dat = NX2.NX2Table('../2008/080424eleventhday_sail.00.csv', (24,4,2008))
 dat.add_rowing_old_format('../2008/Ruderschlaege.csv')
+
+# get our diagnostic plots with
 dat.plot_speeds()
-dat.write_kml('/data/hguenther/Dropbox/Public/test1.kml')
+# or
+dat.plot_course()
+
+# other helpful commands
+dat.keys()
+plt.clf()  # clear figure in interactive plotting
+plt.plot(dat.TIME, dat.COG)
+
