3c37dbed1786fc8f228a8f570735ab1fa5570467
hamogu <hgunther@mit.edu>
2020-06-14 17:06:39 -0400

    pep8

--- utils.py
+++ utils.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
 
-import sys
 from glob import glob
-import time
 import os
 import re
 import warnings
 
 import numpy as np
-from scipy.io import readsav
 
 import NX2
+
 
 def date_from_filename(filename):
     f = os.path.basename(filename)
     match = re.match('[0-9]+', f)
     if match:
-        if match.end() ==6:
+        if match.end() == 6:
             year = 2000 + int(f[0:2])
             month = int(f[2:4])
             day = int(f[4:6])
@@ -25,25 +23,29 @@ def date_from_filename(filename):
             month = int(f[4:6])
             day = int(f[6:8])
         else:
-            raise ValueError('Filename '+f+' must start with 6 or 8 digit date.')
+            raise ValueError('Filename ' + f + ' must start with 6 or 8 digit date.')
     else:
-        raise ValueError('Filename '+f+' does not start with the rowing date.')
+        raise ValueError('Filename ' + f + ' does not start with the rowing date.')
     return day, month, year
 
-def NX2_for_year(filelist, rowfile, corr_bsp = 1.):
+
+def NX2_for_year(filelist, rowfile, corr_bsp=1.):
     dat = []
     with warnings.catch_warnings():
         for f in filelist:
-            data = NX2.NX2Table(f, date_from_filename(f), corr_bsp = corr_bsp, verbose = False)
-            data.add_rowing_old_format(rowfile, verbose = False)
+            data = NX2.NX2Table(f, date_from_filename(f),
+                                corr_bsp=corr_bsp, verbose=False)
+            data.add_rowing_old_format(rowfile, verbose=False)
             dat.append(data)
     return dat
-    
+
+
 def merge_NX2list(NX2list):
     merged = NX2list[0]
     for i in np.arange(1, len(NX2list)):
         merged.append(NX2list[i])
     return merged
+
 
 def read_06(datapath):
     with warnings.catch_warnings():
@@ -56,16 +58,18 @@ def read_06(datapath):
         dat0651.append(dat0661)
     return dat0651
 
+
 def read_08(datapath):
     filelist = glob(os.path.join(datapath, '2008', '*csv'))
     rowfile08 = glob(os.path.join(datapath, '2008', 'Ruderschlaege*.csv'))[0]
     filelist.sort()
     filelist.remove(rowfile08)
-    #file almost empty, BSP missing, a few min only
+    # file almost empty, BSP missing, a few min only
     filelist.remove(os.path.join(datapath, '2008','080504sixteenthday_sail.00.csv'))
-    dat08 = NX2_for_year(filelist, rowfile08, corr_bsp = 1./1.18)
+    dat08 = NX2_for_year(filelist, rowfile08, corr_bsp=1. / 1.18)
     d08 = merge_NX2list(dat08)
     return d08
+
 
 def read_11(datapath):
     filelist = glob(os.path.join(datapath, '2011', '*csv'))
@@ -73,9 +77,10 @@ def read_11(datapath):
     filelist.sort()
     filelist.remove(rowfile11)
     filelist.remove(os.path.join(datapath, '2011', 'Test_Hamburg_110415.00.csv'))
-    dat11 = NX2_for_year(filelist, rowfile11, corr_bsp = 1./0.87)
+    dat11 = NX2_for_year(filelist, rowfile11, corr_bsp=1. / 0.87)
     d11 = merge_NX2list(dat11)
     return d11
+
 
 def read_12(datapath):
     filelist = glob(os.path.join(datapath, '2012', '*csv'))
@@ -83,13 +88,6 @@ def read_12(datapath):
     rowfile12 = glob(os.path.join(datapath, '2012', 'Ruderschlaege*.csv'))[0]
     filelist.remove(rowfile12)
     filelist.remove(os.path.join(datapath, '2012', '20120514_TestSystem.00.csv'))
-    dat12 = NX2_for_year(filelist, rowfile12, corr_bsp = 1./0.87)
+    dat12 = NX2_for_year(filelist, rowfile12, corr_bsp=1. / 0.87)
     d12 = merge_NX2list(dat12)
     return d12
-
-
-
-
-
-
-
